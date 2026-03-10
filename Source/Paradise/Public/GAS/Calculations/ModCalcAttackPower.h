4dfb681e7b89a0c7abf5a1d07c1239c6b1c8e273
YouSungMin <xormraoghk@gmail.com>
2026-03-10 16:26:16 +0900

    Feat : MMC 클래스 추가

---
+++ Source/Paradise/Public/GAS/Calculations/ModCalcAttackPower.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "ModCalcAttackPower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARADISE_API UModCalcAttackPower : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+	
+};
